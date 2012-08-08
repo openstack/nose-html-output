e1742b8c74d570e8f348cb782f6ca035f81d443a
Clark Boylan <clark.boylan@gmail.com>
2012-08-08 09:51:15 -0700

    Add setup.py to the project.
    Add a setup.py to the project that will install the HtmlOutput nose
    plugin as a nose plugin called 'html-output' using setuptools
    entrypoints.

--- setup.py
+++ setup.py
@@ -1,2 +1,27 @@
-# TODO :)
-# Be sure to add the entry point for the nose plugin.
+import htmloutput.htmloutput
+import setuptools
+
+setuptools.setup(
+    name="nosehtmloutput",
+    version=htmloutput.htmloutput.__version__,
+    author='Hewlett-Packard Development Company, L.P.',
+    description="Nose plugin to produce test results in html.",
+    license="Apache License, Version 2.0",
+    url="https://github.com/cboylan/nose-html-output",
+    setup_requires=['nose'],
+    install_requires=['nose'],
+    classifiers=[
+        "Environment :: Console",
+        "Topic :: Software Development :: Testing",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python"
+    ],
+    entry_points = {
+        'nose.plugins.0.10': [
+            'html-output = htmloutput.htmloutput:HtmlOutput'
+        ]
+    }
+)
